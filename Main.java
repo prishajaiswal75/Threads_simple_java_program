16a0e0cce5e71529300e4e7ce9a8384fceef59d5
prishajaiswal75 <164599457+prishajaiswal75@users.noreply.github.com>
2025-04-19 18:01:01 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,30 +1,3 @@
-//Main.java
-class MyThread implements Runnable{
-    private int init;
-    private int increment;
-    private int delay;
-
-    public MyThread(int init,int increment,int delay){
-        this.init=init;
-        this.increment=increment;
-        this.delay=delay;
-    }
-    public void run(){
-        for(;;){
-            try{
-                System.out.println(init+ "");
-                init += increment;
-                Thread.sleep(delay);
-
-            }catch(Exception e){
-                e.printStackTrace();
-
-            }
-        }
-
-
-    }
-}
 class Main{
     public static void main(String args[]){
         //Mythread t = new MyThread();
