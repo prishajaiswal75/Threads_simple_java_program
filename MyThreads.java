10506a7701b9e6048c61270283517ef60643b08e
prishajaiswal75 <164599457+prishajaiswal75@users.noreply.github.com>
2025-04-19 18:00:41 +0530

    Update MyThreads.java

--- MyThreads.java
+++ MyThreads.java
@@ -1,2 +1,26 @@
-public class MyThreads {
+class MyThread implements Runnable{
+    private int init;
+    private int increment;
+    private int delay;
+
+    public MyThread(int init,int increment,int delay){
+        this.init=init;
+        this.increment=increment;
+        this.delay=delay;
+    }
+    public void run(){
+        for(;;){
+            try{
+                System.out.println(init+ "");
+                init += increment;
+                Thread.sleep(delay);
+
+            }catch(Exception e){
+                e.printStackTrace();
+
+            }
+        }
+
+
+    }
 }
